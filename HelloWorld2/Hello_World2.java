e6d5c54b9b0295a70940fab71b43ce0e51b1e976
Katelyn Le <16lek65@millshighsch.orf>
2015-08-18 09:15:48 -0700

    Hello World Demonstration
    It was fun

---
+++ HelloWorld2/Hello_World2.java
@@ -0,0 +1,14 @@
+
+/**
+ * First Program, simple print
+ * 
+ * @author Katelyn Le
+ * @version Aug 18, 2015
+ */
+public class Hello_World2
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World! From Katelyn");
+    }
+}
