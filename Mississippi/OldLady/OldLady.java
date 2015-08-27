c8b7269965718ed35bae2a1e704ac63fba79f476
katelynle <16lek65@millshighsch.org>
2015-08-27 08:39:53 -0700

    Mississippi and Old Lady

---
+++ Mississippi/OldLady/OldLady.java
@@ -0,0 +1,58 @@
+
+/**
+ * Write a description of class OldLady here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class OldLady
+{
+   public static void main(String[] args) {
+        Swallow();
+        System.out.println("fly");
+        Why();
+        Swallow();
+        Spider();
+        Why();
+        Swallow();
+        Bird();
+        Catch();
+        Spider();
+        Why();
+        Swallow();
+        Cat();
+        Catch();
+        Spider();
+        Why();
+        Swallow();
+        Horse();
+    }
+
+    public static void Swallow() {
+        System.out.println(" There was an old woman who swallowed a");
+    }
+
+    public static void Why() {
+        System.out.println("I don't know why she swallowed a fly,\nPerhaps she'll die.");
+    }
+
+    public static void Spider() {
+        System.out.println(" spider,\nThat wriggled and jiggled and tickled inside her,\nShe swallowed the spider to catch the fly");
+    }
+
+    public static void Bird() {
+        System.out.println(" bird,\nHow absurd! to swallow a bird,");
+    }
+
+    public static void Catch() {
+        System.out.println("She swallowed the bird to catch the");
+    }
+
+    public static void Cat() {
+        System.out.println("cat,\nImagine that! to swallow a cat,\nShe swallowed the cat to catch the bird,");
+    }
+
+    public static void Horse() {
+        System.out.println(" horse,\nShe's dead—of course!");
+    }
+}
